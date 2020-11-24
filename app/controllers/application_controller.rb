2631fd499277cec3a5306ce848989d1d564f5472
Thomas <th.farre@gmail.com>
2020-11-24 20:12:44 +0100

    add route to footer and current colocation to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  helper_method :current_colocation
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+  end
+
+  private
+
+  def current_colocation
+    current_colocation ||= current_user.colocation
   end
 end
