2849d062e7154da4552f0e97ec2b5086b87ede24
Franck <franck.siguier@gmail.com>
2020-11-25 11:45:21 +0100

    redirect

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
+  skip_before_action :authenticate_user!, only: :home
   def home
+    redirect_to colocation_path(current_colocation) unless current_user.nil?
   end
 end
