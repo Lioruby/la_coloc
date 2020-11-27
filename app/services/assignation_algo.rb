1fc12f0a4699838fc268ef0a02c9165336a2dcf3
Franck <franck.siguier@gmail.com>
2020-11-27 12:30:06 +0100

    Algorithm to attribute a repetitive task for soemeone

--- app/services/assignation_algo.rb
+++ app/services/assignation_algo.rb
@@ -10,8 +10,24 @@ class AssignationAlgo
 
   def call
     hash_working_time = working_time
-    task_for = faineant(hash_working_time)
-    assignation = Assignation.new(task_id: @task.id, user_id: task_for.keys.first.values[0].id)
+    ap hash_working_time
+    task_for = faineants(hash_working_time)
+    ap task_for
+    user1 = User.find(task_for.keys.first)
+    user2 = User.find(task_for.keys.second)
+    if task_for.values.second - task_for.values.first < 20
+      pref_user_1 = preference(user1, @task)
+      ap pref_user_1
+      pref_user_2 = preference(user2, @task)
+      ap pref_user_2
+      if pref_user_2 < pref_user_1
+        assign(user2, @task)
+      else
+        assign(user1, @task)
+      end
+    else
+      assign(user1, @task)
+    end
   end
 
   private
@@ -23,13 +39,29 @@ class AssignationAlgo
       user.tasks.each do |task|
         total_duration += task.duration
       end
-      working_time_for_user[user: user] = total_duration
+      working_time_for_user[user.id] = total_duration
     end
     working_time_for_user
   end
 
-  def faineant(hash_working)
-    ap [hash_working.min_by{|k, v| v}].to_h
-    [hash_working.min_by{|k, v| v}].to_h
+  def faineants(hash_working)
+    first_min = [hash_working.min_by { |a, b| b }].to_h
+    hash_working.delete(first_min.keys.first)
+    second_min = [hash_working.min_by { |a, b| b }].to_h
+
+    hash_mins = first_min.merge(second_min)
+  end
+
+  def preference(user, tache)
+    # renvoie la position de la tache dans les preferences du user
+    position = nil
+    user.preferences.each do |preference|
+      position = preference.position if preference.task_id == tache.id
+    end
+    position
+  end
+
+  def assign(user, tache)
+    Assignation.create(user_id: user.id, task_id: tache.id, date: Date.today)
   end
 end
