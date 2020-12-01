e7fa33c1ea74d753c212828d606b0f4f4816fa75
Thomas <th.farre@gmail.com>
2020-12-01 15:53:09 +0100

    mes task view c'est bon normalement

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,53 +46,53 @@ courses.save!
 aspirateur.save!
 
 
-# assignation1 = Assignation.new(statut: true, date: Date.today)
-# assignation2 = Assignation.new(statut: false, date: Date.today)
-# assignation3 = Assignation.new(statut: false, date: Date.today)
-# assignation4 = Assignation.new(statut: false, date: Date.today)
-# assignation5 = Assignation.new(statut: false, date: "Sat, 28 Nov 2020")
-# assignation6 = Assignation.new(statut: false, date: "Sun, 29 Nov 2020")
-# assignation7 = Assignation.new(statut: false, date: "Mon, 30 Nov 2020")
-# assignation8 = Assignation.new(statut: false, date: "Thu, 1 Dec 2020")
-assignation9 = Assignation.new(statut: false, date: "Wed, 2 Dec 2020")
-assignation10 = Assignation.new(statut: false, date: "Wed, 2 Dec 2020")
+assignation1 = Assignation.new(statut: true, date: Date.today)
+assignation2 = Assignation.new(statut: false, date: Date.today)
+assignation3 = Assignation.new(statut: false, date: Date.today)
+assignation4 = Assignation.new(statut: false, date: Date.today)
+assignation5 = Assignation.new(statut: false, date: "Thu, 1 Dec 2020")
+assignation6 = Assignation.new(statut: false, date: "Wed, 2 Dec 2020")
+assignation7 = Assignation.new(statut: false, date: "Wed, 2 Dec 2020")
+assignation8 = Assignation.new(statut: false, date: "Wed, 2 Dec 2020")
+assignation9 = Assignation.new(statut: false, date: "Thu, 3 Dec 2020")
+assignation10 = Assignation.new(statut: false, date: "Thu, 3 Dec 2020")
 assignation11 = Assignation.new(statut: false, date: "Thu, 3 Dec 2020")
 assignation12 = Assignation.new(statut: false, date: "Thu, 3 Dec 2020")
 # assignation13 = Assignation.new(statut: false, date: "Fri, 4 Dec 2020")
 
-# assignation1.photo.attach(io: URI.open(task_photos[0]), filename: 'photo.jpg', content_type: 'image/jpg')
-# assignation1.task = vaisselle
-# assignation1.user = franck
-# assignation1.save!
+assignation1.photo.attach(io: URI.open(task_photos[0]), filename: 'photo.jpg', content_type: 'image/jpg')
+assignation1.task = vaisselle
+assignation1.user = franck
+assignation1.save!
 
-# assignation2.photo.attach(io: URI.open(task_photos[1]), filename: 'photo.jpg', content_type: 'image/jpg')
-# assignation2.task = ménage
-# assignation2.user = lior
-# assignation2.save!
+assignation2.photo.attach(io: URI.open(task_photos[1]), filename: 'photo.jpg', content_type: 'image/jpg')
+assignation2.task = ménage
+assignation2.user = lior
+assignation2.save!
 
-# assignation3.task = courses
-# assignation3.user = thomas
-# assignation3.save!
+assignation3.task = courses
+assignation3.user = thomas
+assignation3.save!
 
-# assignation4.task = vaisselle
-# assignation4.user = franck
-# assignation4.save!
+assignation4.task = vaisselle
+assignation4.user = franck
+assignation4.save!
 
-# assignation5.task = vaisselle
-# assignation5.user = thomas
-# assignation5.save!
+assignation5.task = vaisselle
+assignation5.user = thomas
+assignation5.save!
 
-# assignation6.task = vaisselle
-# assignation6.user = lior
-# assignation6.save!
+assignation6.task = vaisselle
+assignation6.user = lior
+assignation6.save!
 
-# assignation7.task = vaisselle
-# assignation7.user = franck
-# assignation7.save!
+assignation7.task = vaisselle
+assignation7.user = franck
+assignation7.save!
 
-# assignation8.task = vaisselle
-# assignation8.user = thomas
-# assignation8.save!
+assignation8.task = vaisselle
+assignation8.user = thomas
+assignation8.save!
 
 assignation9.task = vaisselle
 assignation9.user = thomas
