ff60e5b193225803007577de3af32cfc497b8ae9
Franck <franck.siguier@gmail.com>
2020-12-03 15:37:57 +0100

    Seeds done for presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,12 @@ puts '-------------------'
 
 lacoloc = Colocation.create!(name: 'Le Wagon')
 user_images = ["https://avatars1.githubusercontent.com/u/71550958?s=400&u=5d9c2518ae08db91fa3742fff381ec6a364efd1d&v=4","https://avatars3.githubusercontent.com/u/61694823?s=460&u=41edf0546a675388bb39e187a44770b53bf500e0&v=4","https://avatars2.githubusercontent.com/u/71385272?s=460&u=d515496e33750499be8feae11fa4ce6a92613b3d&v=4", "https://cv.jpheos.fr/assets/images/jpheos.jpg"]
-task_photos = ['https://images.unsplash.com/photo-1581622558667-3419a8dc5f83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80','https://images.unsplash.com/photo-1585421514284-efb74c2b69ba?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80']
+task_photos = ['https://images.unsplash.com/photo-1581622558667-3419a8dc5f83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80','https://images.unsplash.com/photo-1585421514284-efb74c2b69ba?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80','https://images.pexels.com/photos/5824498/pexels-photo-5824498.jpeg?auto=compress&cs=tinysrgb&h=650&w=940','https://images.pexels.com/photos/5691488/pexels-photo-5691488.jpeg?auto=compress&cs=tinysrgb&h=650&w=940','https://images.pexels.com/photos/5824498/pexels-photo-5824498.jpeg?auto=compress&cs=tinysrgb&h=650&w=940','https://5.imimg.com/data5/JK/PW/KM/SELLER-37907573/cat-litter-bentonite-pet-litter-bentonite-powder--500x500.png']
 
 
-thomas = User.new(first_name: 'Thomas', email: 'toto@toto.com', password: '123456')
-lior = User.new(first_name: 'Lior', email: 'titi@toto.com', password: '123456')
-franck = User.new(first_name: 'Franck', email: 'tata@toto.com', password: '123456')
+thomas = User.new(first_name: 'Thomas', email: 'th.farre@gmail.com', password: '123456')
+lior = User.new(first_name: 'Lior', email: 'levy.lior.pro@gmail.com', password: '123456')
+franck = User.new(first_name: 'Franck', email: 'franck.siguier@gmail.com', password: '123456')
 
 thomas.photo.attach(io: URI.open(user_images[0]), filename: 'photo.jpg', content_type: 'image/jpg')
 thomas.colocation = lacoloc
@@ -50,17 +50,19 @@ litiere.save!
 
 assignation1 = Assignation.new(statut: true, date: Date.today)
 assignation2 = Assignation.new(statut: false, date: Date.today)
-assignation3 = Assignation.new(statut: false, date: Date.today)
-assignation4 = Assignation.new(statut: false, date: Date.today)
-# assignation5 = Assignation.new(statut: false, date: "Thu, 1 Dec 2020")
-# assignation6 = Assignation.new(statut: false, date: "Wed, 2 Dec 2020")
-# assignation7 = Assignation.new(statut: false, date: "Wed, 2 Dec 2020")
-# assignation8 = Assignation.new(statut: false, date: "Wed, 2 Dec 2020")
-# assignation9 = Assignation.new(statut: false, date: "Thu, 3 Dec 2020")
-# assignation10 = Assignation.new(statut: false, date: "Thu, 3 Dec 2020")
-# assignation11 = Assignation.new(statut: false, date: "Thu, 3 Dec 2020")
-# assignation12 = Assignation.new(statut: false, date: "Thu, 3 Dec 2020")
-# assignation13 = Assignation.new(statut: false, date: "Fri, 4 Dec 2020")
+# assignation3 = Assignation.new(statut: false, date: Date.today)
+# assignation4 = Assignation.new(statut: false, date: Date.today)
+
+
+assignation5 = Assignation.new(statut: true, date: "Thu, 1 Dec 2020")
+assignation6 = Assignation.new(statut: true, date: "Wed, 2 Dec 2020")
+assignation7 = Assignation.new(statut: true, date: "Wed, 2 Dec 2020")
+assignation8 = Assignation.new(statut: true, date: "Wed, 2 Dec 2020")
+assignation9 = Assignation.new(statut: true, date: "Thu, 3 Dec 2020")
+assignation10 = Assignation.new(statut: true, date: "Thu, 3 Dec 2020")
+assignation11 = Assignation.new(statut: true, date: "Thu, 3 Dec 2020")
+assignation12 = Assignation.new(statut: true, date: "Thu, 3 Dec 2020")
+assignation13 = Assignation.new(statut: true, date: "Thu, 3 Dec 2020")
 
 
 assignation1.photo.attach(io: URI.open(task_photos[0]), filename: 'photo.jpg', content_type: 'image/jpg')
@@ -68,54 +70,58 @@ assignation1.task = vaisselle
 assignation1.user = franck
 assignation1.save!
 
-assignation2.photo.attach(io: URI.open(task_photos[1]), filename: 'photo.jpg', content_type: 'image/jpg')
 assignation2.task = ménage
 assignation2.user = thomas
 assignation2.save!
 
-assignation3.task = courses
-assignation3.user = thomas
-assignation3.save!
+# assignation3.task = courses
+# assignation3.user = thomas
+# assignation3.save!
 
-assignation4.task = vaisselle
-assignation4.user = thomas
-assignation4.save!
+# assignation4.task = vaisselle
+# assignation4.user = thomas
+# assignation4.save!
 
-# assignation5.task = vaisselle
-# assignation5.user = thomas
-# assignation5.save!
+assignation5.photo.attach(io: URI.open(task_photos[1]), filename: 'photo.jpg', content_type: 'image/jpg')
+assignation5.task = vaisselle
+assignation5.user = thomas
+assignation5.save!
 
-# assignation6.task = vaisselle
-# assignation6.user = lior
-# assignation6.save!
+assignation6.photo.attach(io: URI.open(task_photos[2]), filename: 'photo.jpg', content_type: 'image/jpg')
+assignation6.task = vaisselle
+assignation6.user = lior
+assignation6.save!
 
-# assignation7.task = vaisselle
-# assignation7.user = franck
-# assignation7.save!
+assignation7.photo.attach(io: URI.open(task_photos[3]), filename: 'photo.jpg', content_type: 'image/jpg')
+assignation7.task = ménage
+assignation7.user = franck
+assignation7.save!
 
-# assignation8.task = vaisselle
-# assignation8.user = thomas
-# assignation8.save!
+assignation8.photo.attach(io: URI.open(task_photos[4]), filename: 'photo.jpg', content_type: 'image/jpg')
+assignation8.task = vaisselle
+assignation8.user = thomas
+assignation8.save!
 
-# assignation9.task = vaisselle
-# assignation9.user = thomas
-# assignation9.save!
+assignation9.photo.attach(io: URI.open(task_photos[5]), filename: 'photo.jpg', content_type: 'image/jpg')
+assignation9.task = litiere
+assignation9.user = thomas
+assignation9.save!
 
-# assignation10.task = ménage
-# assignation10.user = franck
-# assignation10.save!
+assignation10.task = ménage
+assignation10.user = franck
+assignation10.save!
 
-# assignation11.task = litiere
-# assignation11.user = thomas
-# assignation11.save!
+assignation11.task = litiere
+assignation11.user = thomas
+assignation11.save!
 
-# assignation12.task = courses
-# assignation12.user = lior
-# assignation12.save!
+assignation12.task = courses
+assignation12.user = lior
+assignation12.save!
 
-# assignation13.task = vaisselle
-# assignation13.user = franck
-# assignation13.save!
+assignation13.task = vaisselle
+assignation13.user = franck
+assignation13.save!
 
 prefthomas1 = Preference.new(position: 0)
 prefthomas1.user = thomas
